db97c72f5ce186772dc69e749d93066fcf9438f0
satomo5 <satriyo55@ymail.com>
2024-09-17 15:21:49 +0700

    feature(ga): add script

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import Script from "next/script";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+				src="https://www.googletagmanager.com/gtag/js?id=G-7Y4YLE68W8"
+				strategy="afterInteractive"
+			/>
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+
+					gtag('config', 'G-7Y4YLE68W8');
+				`}
+			</Script>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
